80d909f0919ad4030fb046fc48204819713c07c4
Long Tran <longhtran91@gmail.com>
2023-02-01 15:19:05 -0600

    update deploy image script with opts and args

--- kubernetes/deploy_image_update.py
+++ kubernetes/deploy_image_update.py
@@ -1,22 +1,36 @@
-#!/bin/python
-
-import sys
+import getopt, sys
 from ruamel.yaml import YAML
 
-def main(argv):
-    with open(argv[0], 'r') as yaml_file:
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "f:i:", ['file=','image='])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print(err)  # will print something like "option -a not recognized"
+        sys.exit(2)
+    deployment_file_path = ''
+    images = {}
+    for o, a in opts:
+        if o in ['-f', '--file']:
+            deployment_file_path = a
+        elif o in ['-i', '--image']:
+            images[a.split(':')[0]] = a.split(':')[1]
+        else:
+            assert False, "unhandled option"
+
+
+    with open(deployment_file_path, 'r') as yaml_file:
         yaml=YAML(typ='rt')
         data = list(yaml.load_all(yaml_file))
         for doc in data:
             if doc['kind'] == 'Deployment':
                 for container in doc['spec']['template']['spec']['containers']:
-                    if container['image'].startswith(argv[1]):
-                        container['image'] = argv[2]
-                        break
-                break
+                    image_name = container['image'].split(':')[0]
+                    if image_name in images.keys():
+                        container['image'] = image_name+":"+images.get(image_name)
 
-    with open(argv[0], 'w') as dump_file:
+    with open(deployment_file_path, 'w') as dump_file:
         yaml.dump_all(data, dump_file)
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    main()
